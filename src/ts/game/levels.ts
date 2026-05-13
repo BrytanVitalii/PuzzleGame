73f09534bb547bd2308c12a80e80488fcff069cd
BrytanVitalii <brytanvitalii08@gmail.com>
2026-05-13 18:31:25 +0300

    Fixed paths (#1)

--- src/ts/game/levels.ts
+++ src/ts/game/levels.ts
@@ -1,7 +1,9 @@
+const BASE_URL = import.meta.env.BASE_URL;
+
 export const puzzleLevels = [
-    { id: 0, imagePath: "/assets/puzzles/artemis.jpg" },
-    { id: 1, imagePath: "/assets/puzzles/mountain-view.jpg" },
-    { id: 2, imagePath: "/assets/puzzles/steve.jpg" },
+    { id: 0, imagePath: `${BASE_URL}assets/puzzles/artemis.jpg` },
+    { id: 1, imagePath: `${BASE_URL}assets/puzzles/mountain-view.jpg` },
+    { id: 2, imagePath: `${BASE_URL}assets/puzzles/steve.jpg` },
 ] as const;
 
 export type Level = typeof puzzleLevels[number];
